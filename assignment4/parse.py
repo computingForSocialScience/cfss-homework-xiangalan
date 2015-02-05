f37d7d781f026f22a86bee5f961df41bad09456a
apple <apple@wireless-s0-no-150-9-237.uchicago.edu>
2015-02-04 21:44:28 -0600

    this is assignment 4

--- assignment4/parse.py
+++ assignment4/parse.py
@@ -1,5 +1,6 @@
 import csv
 import sys
+import matplotlib.pyplot as plt
 
 def readCSV(filename):
     '''Reads the CSV file `filename` and returns a list
@@ -12,5 +13,48 @@ def readCSV(filename):
         lines = list(rdr)
     return(lines)
 
+def get_avg_latlng(filename):
+	lines = readCSV("permits_hydepark.csv")
+	sum_lat = 0
+	sum_lng = 0
+	j = 0
+	for i in lines:
+		if i[-3] == "":
+			j += 1
+			continue
+		sum_lat += float(i[-3])
+		sum_lng += float(i[-2])
+	avg_lat = sum_lat/(len(lines)-j)
+	avg_lng = sum_lng/(len(lines)-j)
+	return avg_lat, avg_lng
+print get_avg_latlng("permits_hydepark.csv")
 
+    
+
+
+def zip_code_barchart(filename):
+	lines = readCSV("permits_hydepark.csv")
+	zipcode = []
+	q = [28,35,42,49,56,63,70,77,84]
+	for i in lines:
+		for p in q:
+			if i[p] != "":
+				zipcode.append(int(i[p][:5]))
+		
+	print zipcode
+	plt.hist(zipcode, bins=400)
+	plt.title("Zip Codes")
+	plt.xlabel("Contractor Zip Code")
+	plt.ylabel("Frequency")
+	
+	plt.savefig("zip_code_fre_distribution.jpg")
+	plt.show()
+	
+
+
+
+if sys.argv[1] == "latlong":
+	get_avg_latlng("permits_hydepark.csv")
+elif sys.argv[1] == "hist":
+	zip_code_barchart("permits_hydepark.csv")
 ### enter your code below
